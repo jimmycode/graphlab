fa4f6657c756f5f085610c6f917db4113923a8f3
Jimmy Wu <jimmywu2011@126.com>
2014-08-16 19:40:26 +0800

    version 0.6

--- apps/prob_kNN/prob_kNN.cpp
+++ apps/prob_kNN/prob_kNN.cpp
@@ -24,31 +24,44 @@ class kNN_program :
     public graphlab::ivertex_program<graph_type, gather_type, message_type>,
     public graphlab::IS_POD_TYPE {
 private:
-    message_type msg;
+    float sp; int iter;
 public:
     void init(icontext_type & context, const vertex_type & vertex, const message_type & msg) {
-        this->msg = msg;
+        sp = msg.s;
+        iter = msg.i;
     }
 
     gather_type gather(icontext_type& context, const vertex_type& vertex, edge_type& edge) const { 
         gather_type* tmp = new gather_type();
         float rand_val = rand() / (float)RAND_MAX;       
         if(rand_val < edge.data().p) {
-            tmp->push_back(edge.source().id());
+            tmp->push_back(vertex_sp(edge.source().id(), sp + edge.data().w));
         }
 
         return *tmp;
     }
 
-       
+    void apply(icontext_type& context, vertex_type& vertex, const gather_type & total) {
+        for(gather_type::size_type i = 0; i < total.size(); i++) {
+            samplings[iter].queue.insert(total[i]);
+        }
+    }
+
 };
 
 int main(int argc, char** argv) {
     graphlab::mpi_tools::init(argc, argv);
     graphlab::distributed_control dc;
 
-    graph_type graph(dc);
-    graph.load("test", line_parser);
+    int nsamp = 0;
+    /* Parse input parameters */
+    graphlab::command_line_options clopts("Welcome to probabilistic kNN");
+    clopts.attach_option("src", src, "source vertex id");
+    clopts.attach_option("nsamp", nsamp, "number of samples");
 
+    graph_type graph(dc);
+    graph.load(argv[1], line_parser);
+
+    graph.signal(src)
     graphlab::mpi_tools::finalize();
 }
--- apps/prob_kNN/prob_kNN.h
+++ apps/prob_kNN/prob_kNN.h
@@ -16,13 +16,14 @@ struct compare_vertex_value {
 };
 typedef std::set<vertex_value, compare_vertex_value> vertex_set;
 
+struct vertex_sp {
+    vid_t id;
+    float sp; // Shortest path in current sampling
+    vertex_sp(vid_t a, float b){id = a; sp = b;}
+};
+
 /* Sampling vector stores Dijkstra samplings */
 struct sampling {
-    struct vertex_sp {
-        vid_t id;
-        float sp; // Shortest path in current sampling
-    };
-
     struct compare_sp {
         bool operator () (const vertex_sp& v1, const vertex_sp& v2) {
             return v1.sp > v2.sp;
@@ -51,15 +52,40 @@ typedef graphlab::empty vertex_type;
 typedef graphlab::distributed_graph<vertex_type, edge_type> graph_type;
 
 /* Message tyep */
-struct message_type {
+class message_type {
+public:
     int i; // index of iteration
     float s; // shortest path from source to endpoint, including the weight of current edge
+
+    message_type & operator+=(const message_type & rhs) {
+        return *this;
+    }
+
+    void save(graphlab::oarchive& oarc) const {
+        oarc << i << s;
+    }
+
+    void load(graphlab::iarchive& iarc) {
+        iarc >> i >> s;
+    }
+
 };
 
-class gather_type : public std::vector<vid_t> {
+class gather_type : public std::vector<vertex_sp> {
 public:
     gather_type & operator+=(const gather_type & rhs) {
         this->insert(this->end(), rhs.begin(), rhs.end());
         return *this;
     }
+
+    void save(graphlab::oarchive& oarc) const {
+        for(uint i = 0; i < this->size(); i++)
+            oarc << this->at(i).id << this->at(i).sp;
+    }
+
+    void load(graphlab::iarchive& iarc) {
+         for(uint i = 0; i < this->size(); i++)
+            iarc >> this->at(i).id >> this->at(i).sp;
+    }
+
 };

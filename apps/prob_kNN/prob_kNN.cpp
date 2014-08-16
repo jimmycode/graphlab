343a5ddce27a55e768d9a5b29bcd60bc7acbf301
Jimmy Wu <jimmywu2011@126.com>
2014-08-16 22:40:16 +0800

    version 0.7

--- apps/prob_kNN/prob_kNN.cpp
+++ apps/prob_kNN/prob_kNN.cpp
@@ -2,12 +2,113 @@
 #include <string>
 #include <iostream>
 #include <stdlib.h>
-#include "prob_kNN.h"
+#include <set>
+#include <vector>
+
+typedef graphlab::vertex_id_type vid_t; // Vertex id
+
+/* Vertex set records the expected reliability of each vertex */
+struct vertex_value {
+    vid_t id;
+    float sum; // sum of all shortest path length
+    int count; // counter of shortest path reached
+
+    bool operator== (const vertex_value& v) {
+        return v.id == this->id;
+    }
+};
+
+struct compare_vertex_value {
+    bool operator () (const vertex_value& v1, const vertex_value& v2) {
+        return v1.sum / v1.count > v2.sum / v2.count;
+    }
+};
+typedef std::set<vertex_value, compare_vertex_value> vertex_set;
+
+struct vertex_sp {
+    vid_t id;
+    float sp; // Shortest path in current sampling
+    vertex_sp(vid_t a, float b){id = a; sp = b;}
+};
+
+/* Sampling vector stores Dijkstra samplings */
+struct sampling {
+    struct compare_sp {
+        bool operator () (const vertex_sp& v1, const vertex_sp& v2) {
+            return v1.sp > v2.sp;
+        }
+    };
+    
+    std::set<vertex_sp, compare_sp> queue;
+    std::set<vid_t> visited;
+};
+typedef std::vector<sampling> sampling_vector;
+
+/* Edge type and vertex type definition */
+struct edge_type{
+    float p; // probability of existence 
+    float w; // weight 
+
+    void save(graphlab::oarchive& oarc) const {
+        oarc << p << w;
+    }
+
+    void load(graphlab::iarchive& iarc) {
+        iarc >> p >> w;
+    }
+};
+typedef graphlab::empty vertex_type;
+typedef graphlab::distributed_graph<vertex_type, edge_type> graph_type;
+
+/* Message tyep */
+class message_type {
+public:
+    int i; // index of iteration
+    float s; // shortest path from source to endpoint, including the weight of current edge
+
+    message_type(){}
+    message_type(int iter, float sp) {
+        i = iter;
+        s = sp;
+    }
+
+    message_type & operator+=(const message_type & rhs) {
+        return *this;
+    }
+
+    void save(graphlab::oarchive& oarc) const {
+        oarc << i << s;
+    }
+
+    void load(graphlab::iarchive& iarc) {
+        iarc >> i >> s;
+    }
+
+};
+
+class gather_type : public std::vector<vertex_sp> {
+public:
+    gather_type & operator+=(const gather_type & rhs) {
+        this->insert(this->end(), rhs.begin(), rhs.end());
+        return *this;
+    }
+
+    void save(graphlab::oarchive& oarc) const {
+        for(uint i = 0; i < this->size(); i++)
+            oarc << this->at(i).id << this->at(i).sp;
+    }
+
+    void load(graphlab::iarchive& iarc) {
+         for(uint i = 0; i < this->size(); i++)
+            iarc >> this->at(i).id >> this->at(i).sp;
+    }
+
+};
 
 /* Input parameter: source vertex id */
 vid_t src;
-/* Set of visited vertices */
-vertex_set visited;
+/* Log of visited vertices */
+vertex_set vertex_log;
 /* Vector of samplings */
 sampling_vector samplings;
 
@@ -43,6 +144,22 @@ public:
     }
 
     void apply(icontext_type& context, vertex_type& vertex, const gather_type & total) {
+        vertex_value tmp;
+        tmp.id = vertex.id();
+        vertex_set::iterator it = vertex_log.find(tmp);
+
+        if(it != vertex_log.end()) {
+            tmp.sum = it->sum + sp;   
+            tmp.count = it->count + 1;
+            vertex_log.erase(it);
+            vertex_log.insert(tmp);
+        }
+        else {
+            tmp.sum = sp;
+            tmp.count = 1;
+            vertex_log.insert(tmp);
+        }
+
         for(gather_type::size_type i = 0; i < total.size(); i++) {
             samplings[iter].queue.insert(total[i]);
         }
@@ -54,25 +171,34 @@ int main(int argc, char** argv) {
     graphlab::mpi_tools::init(argc, argv);
     graphlab::distributed_control dc;
 
+    std::string filename;
     int k = 1;
     int nsamp = 200;
+    std::string exec_type = "synchronous";
     /* Parse input parameters */
     graphlab::command_line_options clopts("Welcome to probabilistic kNN");
+    clopts.attach_option("file", filename, "The input filename (required)");
     clopts.attach_option("src", src, "source vertex id");
     clopts.attach_option("k", k, "number of nearest neighbors");
     clopts.attach_option("nsamp", nsamp, "number of samples");
+    clopts.attach_option("engine", exec_type, "The engine type synchronous or asynchronous");
     clopts.parse(argc, argv);
+
+    graph_type graph(dc);
+    graph.load(filename, line_parser);
+    graph.finalize();
+    graphlab::omni_engine<kNN_program> engine(dc, graph, exec_type, clopts);
 
     bool converged = false;
     while(!converged) {
         /* Run one iteration of vertex program for all samplings*/
         for(sampling_vector::size_type i = 0; i < samplings.size(); i++) {
             if(!samplings[i].queue.empty()) {
-                
+                vid_t next_vid = samplings[i].queue.begin()->id;
+                engine.signal(next_vid, message_type(i, samplings[i].queue.begin()->sp));
             }
         }
     }
-    graph_type graph(dc);
 
     graphlab::mpi_tools::finalize();
 }

877ca188c41bd3a7ba83ed157ac605805135e2e0
jimmy <jimmy@jimmy-HP-Compaq-Elite-8300-SFF.(none)>
2014-08-20 13:58:58 +0800

    some dir cleaned

--- exprm/roadNet_test/roadNet_test.sh
+++
@@ -1,3 +0,0 @@
-./test_CA.sh > roadNet_CA.log
-./test_PA.sh > roadNet_PA.log
-./test_TX.sh > roadNet_TX.log
--- exprm/test.sh
+++
@@ -1,1 +0,0 @@
-~/graphchi-cpp/bin/myapps/reachability file ~/datasets/twitter_rv.net source 6074952 dest 61524263

d94087654d31a1ceb71e2b2c707edf01bd978f86
jimmy <jimmy@jimmy-HP-Compaq-Elite-8300-SFF.(none)>
2014-08-20 15:24:37 +0800

    cleaned

--- apps/prob_kNN/test.sh
+++
@@ -1,1 +0,0 @@
-mpiexec -n 1 ~/graphlab/debug/apps/prob_kNN/prob_kNN --file ~/datasets/roadNet/roadNet-CA.txt.prob --src 419 --k 20 --nsamp 200 --window 200 --dist_det 0.2 --engine 'synchronous' --ncpus 4  

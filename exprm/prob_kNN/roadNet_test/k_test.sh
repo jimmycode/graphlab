f350450ed4b7a2ae135eec6f6d43a4a6b84a68fc
jimmy <jimmy@jimmy-HP-Compaq-Elite-8300-SFF.(none)>
2014-08-21 02:22:04 +0800

    k test added

---
+++ exprm/prob_kNN/roadNet_test/k_test.sh
@@ -0,0 +1,22 @@
+~/graphlab/debug/apps/prob_kNN/prob_kNN_mi --file ~/datasets/roadNet/roadNet-CA.txt.prob --dist_det 0.3 --engine "synchronous" --ncpus 8 <<EOF
+61 10 200
+61 20 200
+61 30 200
+61 40 200
+61 50 200
+61 60 200
+61 70 200
+61 80 200
+61 90 200
+61 100 200
+61 110 200
+61 120 200
+61 130 200
+61 140 200
+61 150 200
+61 160 200
+61 170 200
+61 180 200
+61 190 200
+61 200 200
+EOF
---
+++ exprm/prob_kNN/roadNet_test/test.sh
@@ -0,0 +1,3 @@
+~/graphlab/debug/apps/prob_kNN/prob_kNN_mi --file ~/datasets/roadNet/roadNet-CA.txt.prob --dist_det 0.3 --engine "synchronous" --ncpus 8 <<EOF
+61 10 200
+EOF
